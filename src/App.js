bdf8ca642f5c9b3ac5b8f7aaefed3309671a5eaa
Simon Jobling <simonj@asos.com>
2019-05-17 17:10:05 +0100

    Change intro text

--- src/App.js
+++ src/App.js
@@ -7,17 +7,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Kick Off Times</h2>
+        <p>Start times for sport events</p>
       </header>
     </div>
   );
