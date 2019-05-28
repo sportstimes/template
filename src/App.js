f7eae65f32c4eaeca482b848f5ef5de263aaf3de
Simon Jobling <simonj@asos.com>
2019-05-28 18:27:26 +0100

    Rename Explain class names

--- src/App.js
+++ src/App.js
@@ -9,11 +9,11 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <h2>Kick Off Times</h2>
         <p>Start times for sport events using open data formats</p>
-        <ul className="explain--group">
-          <li className="explain--microformats"><strong>Microformats</strong> for people</li>
-          <li className="explain--ics"><strong><abbr title="">ICS</abbr></strong> for calendars</li>
-          <li className="explain--rss"><strong><abbr title="Really Simple Syndication">RSS</abbr></strong> for sharing</li>
-          <li className="explain--jsonld"><strong><abbr title="JavaScript Object Notation Linked Data">JSON-LD</abbr></strong> for <abbr title="Search Engine Results Page">SERP</abbr></li>
+        <ul className="Explain-group">
+          <li className="Explain-microformats"><strong>Microformats</strong> for people</li>
+          <li className="Explain-ics"><strong><abbr title="">ICS</abbr></strong> for calendars</li>
+          <li className="Explain-rss"><strong><abbr title="Really Simple Syndication">RSS</abbr></strong> for sharing</li>
+          <li className="Explain-jsonld"><strong><abbr title="JavaScript Object Notation Linked Data">JSON-LD</abbr></strong> for <abbr title="Search Engine Results Page">SERP</abbr></li>
         </ul>
       </header>
     </div>
